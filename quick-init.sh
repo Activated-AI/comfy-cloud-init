cee472cdf5a5ae079e969718b35795dd868f674c
Rob Neuhaus <rrenaud@gmail.com>
2024-09-18 18:38:14 +0000

    Update quick-init.sh script

--- quick-init.sh
+++ quick-init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-alias less=more
-git config --global user.email "rrenaud@gmail.com
+apt-get update
+apt-get install less
+
+git config --global user.email "rrenaud@gmail.com"
 git config --global user.name "Rob Neuhaus"
 pip install transformers datasets tokenizers
