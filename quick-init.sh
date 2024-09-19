2b75a2db51a66c422d405b71983f31be8f980b01
Rob Neuhaus <rrenaud@gmail.com>
2024-09-19 16:12:03 +0000

    added vim and unit testing

--- quick-init.sh
+++ quick-init.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 apt-get update
-apt-get install less
+apt-get install less vim
+
 
 git config --global user.email "rrenaud@gmail.com"
 git config --global user.name "Rob Neuhaus"
-pip install transformers datasets tokenizers
+pip install transformers datasets tokenizers pytest matplotlib
