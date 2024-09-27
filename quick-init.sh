fc37fc75aa0de93e59b6d97d9cdfafba8f4efd30
Rob Neuhaus <rrenaud@gmail.com>
2024-09-27 17:13:26 +0000

    added parallelization to init

--- quick-init.sh
+++ quick-init.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+pip install transformers datasets tokenizers pytest matplotlib &
+PIP_PID=$!
+
 apt-get update
-apt-get install less emacs vim
+apt-get -y install less emacs vim tmux &
+APT_PID=$!
 
 
 git config --global user.email "rrenaud@gmail.com"
 git config --global user.name "Rob Neuhaus"
-pip install transformers datasets tokenizers pytest matplotlib
+
+wait $APT_PID $PIP_PID
+
+echo "All installations completed."
